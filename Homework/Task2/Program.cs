af7ea1d14f6bc199ea0e23024ee1b95f1368430c
Samir <camir05r@mail.ru>
2023-02-11 19:17:38 +0300

    add HomeworkTask2

---
+++ Homework/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a");
+int a = int.Parse(ReadLine());
+
+WriteLine("Введите число b");
+int b = int.Parse(ReadLine());
+
+WriteLine("Введите число c");
+int c = int.Parse(ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    WriteLine($"max = {a}");
+    else
+    WriteLine($"max = {c}");
+}
+else
+    if (b > c)
+    WriteLine($"max = {b}");
+    else
+    WriteLine($"max = {c}");
