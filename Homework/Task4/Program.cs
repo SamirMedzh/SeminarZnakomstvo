391881360aa1e3a3aaf59d067cfb92b0a03ca2ef
Samir <camir05r@mail.ru>
2023-02-11 20:01:20 +0300

    add HomeworkTask4

---
+++ Homework/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+WriteLine("Введите число N");
+int N = int.Parse(ReadLine()!);
+
+int index = 0;
+
+while (N > index)
+{
+    if (index%2 == 0)
+     Write(index);
+     index++;
+}
+
+
+
+
+
+
