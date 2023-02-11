db162088f8b3e62917aae6152955ed773deae2a5
Samir <camir05r@mail.ru>
2023-02-11 21:27:42 +0300

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a*a == b? "b является квадр а" : 
+          b*b == a? "а является квадратом b"
+          : "не являются квадр друг друга");
