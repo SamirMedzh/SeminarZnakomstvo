40ee73cdad5b7ec09aa4ba2e72d5fe6a3a3bdc50
Samir <camir05r@mail.ru>
2023-02-11 18:06:16 +0300

    add  task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Являеься ли число а квадратом числа b? ");
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+if (b * b == a)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
