27b4ce08d4f3b469e4bfe41d3551d975f697cbec
Samir <camir05r@mail.ru>
2023-02-11 21:00:15 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a%b==0?"кратно":$"некратно, остаток {a%b}");
