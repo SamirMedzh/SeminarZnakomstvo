52f2ec217698320d5b741bf2245daade3e141fac
Samir <camir05r@mail.ru>
2023-02-11 20:37:24 +0300

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+int number = new Random().Next(10,100);
+WriteLine(number);
+int a1 = number/10;
+int a2 = number%10;
+
+//int max = a1 > a2?a1:a2; 
+//WriteLine(max);
+
+WriteLine(a1>a2?a1:a2);
+
+//if(a1 > a2)
+//{
+//    WriteLine(a1);
+//}
+//else
+//{
+//    WriteLine(a2);
+//}
+
+//Также можно записать
+//WriteLine(number/10>number%10?number/10:number%10);
