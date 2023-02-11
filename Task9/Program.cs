a20d1cf5d6791a7f0879f09dd1d02384fdab318b
Samir <camir05r@mail.ru>
2023-02-11 21:07:38 +0300

    add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a == b?"=": a > b? ">" : "<");
