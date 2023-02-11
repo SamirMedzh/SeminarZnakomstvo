a3e28164f28baddee776cc00936311e7b49198d2
Samir <camir05r@mail.ru>
2023-02-11 20:49:26 +0300

    add Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+int number = new Random().Next(100,1000);
+WriteLine(number);
+
+int first = number/100;
+int second = number%10;
+
+Write(first);
+WriteLine(second);
