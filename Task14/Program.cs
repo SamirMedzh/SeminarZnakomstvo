ee879e59d3942167f9676c7f513ed56e81f3e14e
Samir <camir05r@mail.ru>
2023-02-12 19:14:51 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+Clear();
+
+
+Write("Введите координаты x: ");
+int x = int.Parse(ReadLine()!);
+Write("Введите координаты y: ");
+int y = int.Parse(ReadLine()!);
+
+if (x == 0 || y == 0)
+{
+   WriteLine("Error");
+   return;
+}
+if (x > 0 && y > 0)
+{
+   WriteLine("I четверть");
+   return;
+}
+if (x < 0 && y > 0)
+{
+   WriteLine("II четверть");
+   return;
+}
+if (x < 0 && y < 0)
+{
+   WriteLine("III четверть");
+   return;
+}
+if (x > 0 && y < 0)
+{
+   WriteLine("IV четверть");
+   return;
+}
