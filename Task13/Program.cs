e092a240ced3b0774bb1783c8bb59a3afd2bf64d
Samir <camir05r@mail.ru>
2023-02-12 18:55:48 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Являеься номер дня недели? ");
+int a = int.Parse(ReadLine()!);
+
+switch(a)
+{
+     case 1:
+     {
+        WriteLine("Понедельник");
+        break;
+     }
+     case 2:
+     {
+        WriteLine("Вторник");
+        break;
+     }
+     case 3:
+     {
+        WriteLine("Среда");
+        break;
+     }
+     case 4:
+     {
+        WriteLine("Четверг");
+        break;
+     }
+     case 5:
+     {
+        WriteLine("Пятница");
+        break;
+     }
+     case 6:
+     {
+        WriteLine("Суббота");
+        break;
+     }
+     case 7:
+     {
+        WriteLine("Воскресенье");
+        break;
+     }
+     default:
+     {
+        Write("Данного дня недели не существует ");
+        break;
+     }   
+}
+
+
+
+
