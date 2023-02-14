4d58db6c052dc108bb30593404f1d8aac520d7c6
Samir <camir05r@mail.ru>
2023-02-14 20:18:06 +0300

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N)
+// и выдает таблицу квадратов чисел от 1 дор N
+
+using static System.Console;
+Clear();
+int i = 1;
+
+Write("Введите число для вывода квадратов от 1 до числа: ");
+int N = int.Parse(ReadLine()!);
+// int.TryParse(ReadLine(), out int N);
+
+
+
+ while( i <= N)
+ {
+     WriteLine($"Квадрат числа {i} = {Math.Pow(i, 2)}");
+     i++;
+ }
