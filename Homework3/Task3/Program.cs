64e37fd61e1355aa88e898adfe99b2c00e8da003
Samir <camir05r@mail.ru>
2023-02-14 20:51:37 +0300

    add Homework3 Task3

---
+++ Homework3/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N)
+// и выдает таблицу кубов чисел от 1 дор N
+
+using static System.Console;
+Clear();
+int i = 1;
+
+Write("Введите число для вывода кубов от 1 до числа: ");
+int N = int.Parse(ReadLine()!);
+// int.TryParse(ReadLine(), out int N);
+
+
+
+ while( i <= N)
+ {
+     WriteLine($"Куб числа {i} = {Math.Pow(i, 3)}");
+     i++;
+ }
