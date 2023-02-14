56bb00b96f10a2f9bd050b26764e08b2ffa76a8f
Samir <camir05r@mail.ru>
2023-02-14 19:27:08 +0300

    add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты 2 точек
+// и находит расстояние между ними в 2D пространстве;
+
+using static System.Console;
+Clear();
+
+
+Write("Точка А -> Введите координаты x: ");
+int ax = int.Parse(ReadLine()!);
+Write("Точка А -> Введите координаты y: ");
+int ay = int.Parse(ReadLine()!);
+Write("Точка B -> Введите координаты x: ");
+int bx = int.Parse(ReadLine()!);
+Write("Точка B -> Введите координаты y: ");
+int by = int.Parse(ReadLine()!);
+
+// Math.Pow() - возведение в степень
+// Math.Sqrt() - извлекает квадратный корень
+// С(квадрат) = А(квадрат) + В(квадрат)  -   Теорема Пифагора
+
+double length = Math.Sqrt(Math.Pow((ax - bx),2) + Math.Pow((ay - by),2));
+WriteLine(length);
