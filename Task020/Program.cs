aca0e798363db62a34296e7e2ebd9ea7eafda4af
Samir <camir05r@mail.ru>
2023-02-15 16:46:06 +0300

    add Task020

---
+++ Task020/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведения чисел от 1 до N
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N ");
+int N = int.Parse(ReadLine()!);
+int N2 = sum(N );
+WriteLine($"Сумма произседений чисел от 1 до {N} = {N2}");
+
+
+int sum (int A)
+{
+    int result = 1;
+    while (A > 1)
+    {
+        result = result * A;
+        A--;
+    }
+    return result;
+}
