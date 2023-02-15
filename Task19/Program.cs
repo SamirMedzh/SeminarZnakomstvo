f3227620cac73337a4419767e9f8e54f905f0658
Samir <camir05r@mail.ru>
2023-02-15 15:41:51 +0300

    add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,46 @@
+﻿// Написать программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе
+
+using static System.Console;
+Clear();
+Write("Напишите число: ");
+int number = int.Parse(ReadLine()!);
+int sum = GetSum(number);
+WriteLine($"Количество цифр = {sum}");
+
+
+
+
+
+
+int GetSum(int A)
+{
+    int result = 0;
+    
+    while (A > 1)
+    {
+        result = result + 1;
+        A = A / 10; 
+    }
+
+    return result;
+    
+}
+
+
+// int[] array = { 1, 12, 23, 34, 43, 43, 33, 66 };
+
+// int n = array.Length;
+// int find = 43;
+
+// int index = 0;
+
+// while (index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
