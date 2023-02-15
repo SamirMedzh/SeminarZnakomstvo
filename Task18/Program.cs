e11465bbbe0c1e8c7ed7e59d395d4fb248165231
Samir <camir05r@mail.ru>
2023-02-15 14:51:30 +0300

    add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает число А и 
+// выдает сумму чисел от 1 до А
+
+using static System.Console;
+Clear();
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+int sum = GetSum(number);
+WriteLine($"Sum number 1..A = {sum}");
+
+
+
+
+int GetSum(int A)
+{
+    int result = 0;
+    while (A>0)
+    {
+        result += A;
+        A--;
+    }
+    return result;
+}
