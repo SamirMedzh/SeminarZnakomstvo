75a95464f292385e2d21a0d7ee05690e6cdbf930
Samir <camir05r@mail.ru>
2023-02-15 18:47:00 +0300

    add Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.[1,0,1,1,0,1,0,0]
+
+using static System.Console;
+Clear();
+
+int[] array = InitBinaryArray();
+PrintArray(array);
+
+int[] InitBinaryArray()
+{
+    int[] res = new int[8];
+    
+    for (int i = 0; i < res.Length; i++ )
+    {
+        Random ran = new Random();
+        res[i] =ran.Next(0, 2);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++ )
+    {
+        Write($"{arr[i]}");
+    }
+}
