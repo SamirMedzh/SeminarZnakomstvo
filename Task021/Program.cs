88c02523de7d0642f8f74eb61a586c6801b2bbc0
Samir <camir05r@mail.ru>
2023-02-15 17:02:57 +0300

    add Task021

---
+++ Task021/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт сумму чисел от 1 до N, на которые N делится без остатка
+
+
+using static System.Console;
+Clear();
+Write("Напишите число N: ");
+int number = int.Parse(ReadLine()!);
+int sum = GetSum(number);
+WriteLine($"сумму чисел = {sum}");
+
+
+
+
+
+
+int GetSum(int A)
+{
+    int result = 0;
+    int temp = A;
+
+    while (A > 0)
+    {
+        if (temp % A == 0)
+        {   
+            result += A;
+        }
+        
+        A--; 
+    }
+
+    return result;
+    
+}
