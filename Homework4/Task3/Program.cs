0792f860fdcac8225d3eabee55332b32ac86cdaf
Samir <camir05r@mail.ru>
2023-02-16 21:14:55 +0300

    add Homework4task3

---
+++ Homework4/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу реализующая метод, который формирует
+// массив случайных целых чисел из 8 элементов и выводит их на экран
+
+using static System.Console;
+Clear();
+
+int[] ARR = GetArray(8);
+PrintArray(ARR);
+
+
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++ )
+    {
+        array[i] = new Random().Next(10);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Write($"{array[i]}");
+    }
+}
