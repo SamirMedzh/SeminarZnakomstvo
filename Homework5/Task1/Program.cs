9af027ece72186e6a8786a6ae124098c6312cf78
Samir <camir05r@mail.ru>
2023-02-20 21:19:31 +0300

    Homework5Task1

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу реализующая методы формирования массива,
+// заполненного случайными положительными 3x значчислами и подсчета
+// кол-ва четных чисел массива
+
+using static System.Console;
+Clear();
+
+int[] arr = GetArray(1);
+
+PrintArray(arr);
+WriteLine();
+WriteLine(ResultEven(arr));
+
+
+
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(100, 1000);
+    }
+
+    return resultArray;
+}
+
+int ResultEven (int[] InArray)
+{
+    int sum = 0;
+    for (int i = 0; i < InArray.Length; i++)
+    {
+        if (InArray[i] % 2 == 0)
+        {
+            sum++;
+        } 
+    }  
+    return sum;
+}
+
+void PrintArray(int[] inArray)                  // Этот метод, красиво выводит массив в квадр скобках через запятые,- 1 поставил чтоб последний элемент выводился без запятой
+{                                               // послед элемент вставил во второй ВРАЙТ и закрыл квадр скобкой                                         
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+           Write($"{inArray[i]},"); 
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
