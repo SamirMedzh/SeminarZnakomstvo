12416a04c117a6006be763dd17c46fa2599cb4a0
Samir <camir05r@mail.ru>
2023-02-22 17:28:07 +0300

    Task033

---
+++ Task033/Program.cs
@@ -0,0 +1,23 @@
+﻿// 6 знаков в билете, является ли он счастливым
+// первая сумма 3 знаков должна бфыть равна сумме вторых 3 зн
+
+using static System.Console;
+Clear();
+
+
+Write("Введите номер билета : ");
+string ticket = ReadLine();
+
+bool Ticket(int numb)
+{
+    int a = numb / 100000;
+    int b = (numb / 10000) % 10;
+    int c = (numb / 1000) % 10;
+    int d = (numb / 1000) / 100;
+    int e = (numb / 1000) / 10 % 10;
+    int f = (numb / 1000) % 10;
+
+    return(a + b + c == d + e + f);
+}
+
+
