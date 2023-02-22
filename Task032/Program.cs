e4209381cf1c02bc40e60f6e972b1b2339343b12
Samir <camir05r@mail.ru>
2023-02-22 16:18:04 +0300

    Task032

---
+++ Task032/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+using static System.Console;
+Clear();
+
+Write("Введите число для перевода в двоичную систему: ");
+int Number = int.Parse(ReadLine()!);
+
+// WriteLine($"{Number%2}");
+// WriteLine($"{Number/2}");
+
+WriteLine(BinaryNumber(Number));
+
+string BinaryNumber (int numb)
+{
+    if(numb == 0) return "0";
+    string result = string.Empty;
+    int rem;
+    while (numb>0)
+    {
+        rem = numb%2;
+        numb /=2;
+        result = rem.ToString() + result;
+    }
+    return result;
+}
