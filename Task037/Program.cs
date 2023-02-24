63bdf9eb36fe73221f1a32e7af14a468bd95e6ab
Samir <camir05r@mail.ru>
2023-02-24 22:56:51 +0300

    Task037

---
+++ Task037/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по 
+// формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+using static System.Console;
+Clear();
+
+int[,] Array = GetArrayMatr(3, 4);
+
+PrintArrayMatr(Array);
+
+
+
+int[,] GetArrayMatr(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArrayMatr(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+
+
