0aabbb95bc30d0869a5c9834c528f97902ee0ecf
Samir <camir05r@mail.ru>
2023-02-25 20:42:07 +0300

    Homework7Task1

---
+++ Homework7/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу реализующую метод, который возвращает массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите размер массива и диапазон через пробел ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+double[,] MatrixArray = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+
+PrintMatrixArray(MatrixArray);
+
+
+
+double[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    double[,] resultArray = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return resultArray;
+}
+
+
+
+void PrintMatrixArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]:f1} ");
+        }
+        WriteLine();
+    }
+}
