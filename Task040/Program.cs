a0fa34a08ecfdbe547e84993d585fbcc3bd2a694
Samir <camir05r@mail.ru>
2023-02-25 16:36:55 +0300

    Task040

---
+++ Task040/Program.cs
@@ -0,0 +1,23 @@
+﻿//Какого цвета клетка шахматной доски, если начинается с черной
+
+
+using static System.Console;
+Clear();
+
+Write("Введите номер доски ");
+
+string parametrs = ReadLine();
+int par1 = parametrs[0];
+int par2 = parametrs[1];
+
+WriteLine(par1);
+WriteLine(par2);
+
+if ((par1 + par2)%2 == 0)
+{
+    Write("Чёрное ");
+}
+else
+{
+    Write("Белое ");
+}
